b80901e0d8fa4eff8a3fbe0828338ce93651c087
dongamanan <dongadaya2009@gmail.com>
2025-05-28 14:17:33 +0530

    Add files via upload

---
+++ pr-3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int size,sum;
+	
+	printf("Enter the array's row & column size:");
+	scanf("%d",&size);
+	
+	printf("Enter array's element:\n");
+	int a[size][size];
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			printf("a[%d][%d]=",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	printf("\nthe transpose matrix of an array:\n");
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
